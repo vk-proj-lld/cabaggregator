1549394154f791052538b237377dd4493881153c
Vajahat Kareem <vajahatk@dotpe.in>
2022-02-01 01:40:06 +0530

    improved fileOut

--- entities/out/fileout.go
+++ entities/out/fileout.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sync"
 	"sync/atomic"
 )
 
 type fileout struct {
+	mu     sync.Mutex
 	writer io.WriteCloser
 }
 
@@ -17,10 +19,14 @@ var counter uint32
 func NewFileOut() IOout {
 	w, err := os.OpenFile(
 		fmt.Sprintf("./out.%d.txt", atomic.AddUint32(&counter, 1)),
-		os.O_WRONLY,
-		os.ModeAppend,
+		os.O_RDWR|os.O_CREATE|os.O_APPEND|os.O_TRUNC|os.O_SYNC,
+		0644,
 	)
-	fmt.Println(err)
+	if err == nil {
+		fmt.Println(w.Name())
+	} else {
+		fmt.Println(err)
+	}
 	return &fileout{
 		writer: w,
 	}
@@ -35,6 +41,7 @@ func (f *fileout) Write(data ...interface{}) {
 			raw = append(raw, r...)
 		}
 	}
+	raw = append(raw, '\n')
 	f.writer.Write(raw)
 }
 
